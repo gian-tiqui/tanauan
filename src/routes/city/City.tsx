07553f0d5807574f073a415fa392c2b247d88d42
Prodigious <cisprodigious@gmail.com>
2024-02-08 11:02:43 +0800

    asjbdakjsdna

--- src/routes/city/City.tsx
+++ src/routes/city/City.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
+interface DaVid {
+  title: string;
+  uri: string;
+}
+
 const City = () => {
+  const [daVid, setDaVid] = useState<DaVid | undefined>(undefined);
+
+  useEffect(() => {
+    setDaVid(undefined);
+  }, []);
+
+  useEffect(() => {
+    console.log(daVid);
+  }, [daVid]);
+
   return (
     <div className="w-full">
       <video
