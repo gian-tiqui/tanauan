309a975223a7cb985893030f63a97c0e1521a9f3
gian <michaelgiantiqui3@gmail.com>
2024-02-10 22:55:13 +0800

    ajksbndajknbds

--- src/routes/city/City.tsx
+++ src/routes/city/City.tsx
@@ -16,19 +16,7 @@ const City = () => {
     console.log(daVid);
   }, [daVid]);
 
-  return (
-    <div className="w-full">
-      <video
-        autoPlay
-        muted
-        loop
-        className="w-full h-auto object-cover"
-        style={{ height: "508px" }}
-      >
-        <source src="../../j.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );
+  return <div className="w-full"></div>;
 };
 
 export default City;
