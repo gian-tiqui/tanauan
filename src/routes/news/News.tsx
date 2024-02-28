2eebbda278b4e26e176eb4cae925e08bc29eb889
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-02-28 20:02:29 +0800

    kandsklna

--- src/routes/news/News.tsx
+++ src/routes/news/News.tsx
@@ -1,10 +1,12 @@
-import { mNews } from "../home/components/NewsCarousel";
+import { mNews } from "../../large-strings-folder/Dummy";
+import Divider from "../home/components/Divider";
 import NewsCard from "./components/NewsCard2";
 
 const News = () => {
   return (
-    <div className="container px-3 mx-auto">
-      <div className="flex flex-wrap justify-center gap-5">
+    <div className="container px-3 mx-auto mt-8">
+      <Divider text="News" />
+      <div className="flex flex-wrap justify-center gap-5 mt-10">
         {mNews.map((news) => (
           <NewsCard key={news.header} {...news} />
         ))}
