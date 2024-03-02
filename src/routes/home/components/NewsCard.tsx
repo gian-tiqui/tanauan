c6467acc8d5de15af8b4055fb7b7266b2f53aa01
gian <michaelgiantiqui3@gmail.com>
2024-03-02 17:36:48 +0800

    news carousel done, news page next

--- src/routes/home/components/NewsCard.tsx
+++ src/routes/home/components/NewsCard.tsx
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { News } from "./NewsCarousel";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-const NewsCard: React.FC<News> = ({ header, date, link, imageURI }) => {
+const NewsCard: React.FC<News> = ({ title, date, link, featuredMedia }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [image, setImage] = useState<string | undefined>(undefined);
+  const [formattedDate, setFormattedDate] = useState<string>("");
+
+  useEffect(() => {
+    const getImage = async () => {
+      try {
+        const res = await axios.get(featuredMedia);
+        const data = res.data;
+        setImage(data.guid.rendered);
+      } catch (error) {
+        console.error("Error fetching image:", error);
+      }
+    };
+
+    if (featuredMedia) {
+      getImage();
+    }
+  }, [featuredMedia]);
+
+  useEffect(() => {
+    const formattedDate = formatDate(date);
+    setFormattedDate(formattedDate);
+  }, [date]);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  const formatDate = (dateString: string): string => {
+    const formattedDate = new Date(dateString).toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+    return formattedDate;
+  };
+
   return (
     <div className="relative overflow-hidden border rounded-lg shadow-xl border-2xl">
-      <div
-        className="h-32 bg-center bg-cover sm:h-32 md:h-40"
-        style={{ backgroundImage: `url(${imageURI})` }}
-      ></div>
+      {(image || image === "") && (
+        <div className="h-32 bg-center bg-cover sm:h-32 md:h-40">
+          <img
+            src={image}
+            alt={title.rendered}
+            className={`w-full h-full object-cover ${
+              imageLoaded ? "block" : "hidden"
+            }`}
+            onLoad={handleImageLoad}
+          />
+          {!imageLoaded && (
+            <div className="flex items-center justify-center w-full h-full bg-gray-300">
+              <img src="loading.gif" alt="Loading" className="w-8 h-8" />
+            </div>
+          )}
+        </div>
+      )}
       <div className="flex flex-col p-4 bg-white">
         <p className="text-xs font-bold truncate sm:text-md md:text-lg lg:text-xl">
-          {header}
+          {title.rendered}
         </p>
-        <p className="text-xs text-gray-600 sm:text-sm">{date}</p>
+        <p className="text-xs text-gray-600 sm:text-sm">{formattedDate}</p>
         <Link
           className="self-end w-20 py-1 mt-4 text-xs text-center text-white bg-red-500 border rounded-md sm:px-1 sm:truncate md:font-bold lg:font-bold hover:bg-red-600 sm:w-16 md:w-24 lg:28 sm:text-xs md:text-sm lg:text-md"
           to={link}
--- src/routes/home/components/NewsCarousel.tsx
+++ src/routes/home/components/NewsCarousel.tsx
@@ -1,74 +1,116 @@
-import { useRef, useState } from "react";
+import { useState, useEffect } from "react";
 import NewsCard from "./NewsCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/bundle";
-import { mNews } from "../../../large-strings-folder/Dummy";
+import axios, { AxiosResponse } from "axios";
+import News from "../../news/News";
 
 export interface News {
-  header: string;
+  title: { rendered: string };
   date: string;
   link: string;
-  imageURI: string;
+  content: { rendered: string };
+  featuredMedia: string;
 }
 
-const NewsCarousel = () => {
-  const [news] = useState<News[]>(mNews);
+const MAX_PAGE_NUMS = 1;
 
-  const newsRef = useRef(news);
+const NewsCarousel = () => {
+  const [news, setNews] = useState<News[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let allData: News[] = [];
+
+        for (let pageNum = 1; pageNum <= MAX_PAGE_NUMS; pageNum++) {
+          const response: AxiosResponse<News[]> = await axios.get(
+            `https://tanauancity.gov.ph/wp-json/wp/v2/posts?page=${pageNum}`
+          );
+          allData = allData.concat(response.data);
+        }
+
+        const modifiedNews = allData.map((item: any) => ({
+          title: item.title,
+          date: item.date,
+          link: item.link,
+          content: item.content,
+          featuredMedia: item._links["wp:featuredmedia"][0].href,
+        }));
+
+        setNews(modifiedNews);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="container px-4 mx-auto">
       {/* large screens */}
-      <div className="hidden sm:hidden md:block lg:block">
-        <Swiper
-          spaceBetween={15}
-          slidesPerView={3}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-          }}
-          modules={[Autoplay, Pagination, Navigation]}
-          loop={true}
-          className="p-10 mt-10 mySwiper sm:mx-5 md:mx-5 lg:mx-44"
-        >
-          {news.map((ns, index) => (
-            <SwiperSlide key={index}>
-              <NewsCard
-                header={ns.header}
-                date={ns.date}
-                link={ns.link}
-                imageURI={ns.imageURI}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <div className="block sm:block md:hidden lg:hidden">
-        <Swiper
-          spaceBetween={15}
-          slidesPerView={1}
-          autoplay={{
-            delay: 1400,
-            disableOnInteraction: false,
-          }}
-          modules={[Autoplay, Pagination, Navigation]}
-          loop={true}
-          className="p-5 mx-3 mt-10 mySwiper sm:mx-5 md:mx-5 lg:mx-44"
-        >
-          {newsRef.current.map((ns, index) => (
-            <SwiperSlide key={index}>
-              <NewsCard
-                header={ns.header}
-                date={ns.date}
-                link={ns.link}
-                imageURI={ns.imageURI}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      {!loading && news.length > 0 && (
+        <>
+          <div className="hidden sm:hidden md:block lg:block">
+            <Swiper
+              spaceBetween={15}
+              slidesPerView={3}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              modules={[Autoplay, Pagination, Navigation]}
+              loop={true}
+              className="p-10 mt-10 mySwiper sm:mx-5 md:mx-5 lg:mx-44"
+            >
+              {news.map((ns, index) => (
+                <SwiperSlide key={index}>
+                  <NewsCard
+                    title={ns.title}
+                    date={ns.date}
+                    link={ns.link}
+                    featuredMedia={ns.featuredMedia}
+                    content={{ rendered: ns.content.rendered }}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          <div className="block sm:block md:hidden lg:hidden">
+            <Swiper
+              spaceBetween={15}
+              slidesPerView={1}
+              autoplay={{
+                delay: 1400,
+                disableOnInteraction: false,
+              }}
+              modules={[Autoplay, Pagination, Navigation]}
+              loop={true}
+              className="p-5 mx-3 mt-10 mySwiper sm:mx-5 md:mx-5 lg:mx-44"
+            >
+              {news.map((ns, index) => (
+                <SwiperSlide key={index}>
+                  <NewsCard
+                    title={ns.title}
+                    date={ns.date}
+                    link={ns.link}
+                    featuredMedia={ns.featuredMedia}
+                    content={{ rendered: ns.content.rendered }}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </>
+      )}
+      {loading && <p>Loading...</p>}
     </div>
   );
 };
