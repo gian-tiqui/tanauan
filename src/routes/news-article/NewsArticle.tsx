e9f25b66f11d370cd63507782ade441a1548bc85
gian <michaelgiantiqui3@gmail.com>
2024-03-04 00:45:28 +0800

    news article updated

--- src/routes/news-article/NewsArticle.tsx
+++ src/routes/news-article/NewsArticle.tsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { News } from "../home/components/NewsCarousel";
+
+interface Post {
+  title: { rendered: string };
+  content: { rendered: string };
+  date: string;
+}
 
 const NewsArticle = () => {
   const { id } = useParams();
-  const [newsData, setNewsData] = useState<News | null>(null);
+  const [newsData, setNewsData] = useState<Post | null>(null);
   const [loading, setLoading] = useState(true);
+  const [extractedStrings, setExtractedStrings] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -16,6 +22,8 @@ const NewsArticle = () => {
         );
 
         const data = response.data;
+        const extractedContent = extractStrings(data.content.rendered);
+        setExtractedStrings(extractedContent);
         setNewsData(data);
       } catch (error) {
         console.log(error);
@@ -27,20 +35,33 @@ const NewsArticle = () => {
     fetchNews();
   }, [id]);
 
+  const extractStrings = (htmlContent: string): string[] => {
+    const divElements = new DOMParser()
+      .parseFromString(htmlContent, "text/html")
+      .querySelectorAll(".x11i5rnm div");
+    const strings: string[] = [];
+    divElements.forEach((element) => {
+      if (element.textContent) {
+        strings.push(element.textContent.trim());
+      }
+    });
+    return strings;
+  };
+
   if (loading) {
     return <p>Loading...</p>;
   }
 
   return (
-    <div>
-      {newsData ? (
-        <>
-          <h2>{newsData.title.rendered}</h2>
-          <p dangerouslySetInnerHTML={{ __html: newsData.content.rendered }} />
-        </>
-      ) : (
-        <p>No data available</p>
-      )}
+    <div className="flex justify-center">
+      <div className="max-w-lg overflow-hidden bg-white rounded-md shadow-md">
+        <div className="px-6 py-4">
+          <h2 className="mb-2 text-xl font-semibold">
+            {newsData?.title.rendered}
+          </h2>
+          <pre className="text-gray-700">{extractedStrings.join("\n")}</pre>
+        </div>
+      </div>
     </div>
   );
 };
