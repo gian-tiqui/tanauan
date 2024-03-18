588c40a567b7e60c22744ad5d3f24d4f19340e4d
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-03-18 12:07:05 +0800

    you spin my head right round right round when you go down when you go down down

--- src/routes/tourism/destinations/Destinations.tsx
+++ src/routes/tourism/destinations/Destinations.tsx
@@ -25,6 +25,9 @@ interface DelicaciesInterface {
 export const Map = () => {
   return (
     <div className="w-full overflow-hidden rounded-lg">
+      <p className="text-2xl font-bold text-center animate-spin">
+        Map of Tanauan City
+      </p>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3869.73505047557!2d121.01873887456442!3d14.092811589308065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd709208a54f87%3A0x7d349a8ce58c7f8!2s4232%20Talisay%20-%20Tanauan%20Rd%2C%20Talisay%2C%20Batangas!5e0!3m2!1sen!2sph!4v1710214495844!5m2!1sen!2sph"
         allowFullScreen={true}
