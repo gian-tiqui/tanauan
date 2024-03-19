4e66cb884924b4b710f4096cc186dc283ab224c8
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-03-19 11:38:43 +0800

    new map uri added

--- src/routes/tourism/destinations/Destinations.tsx
+++ src/routes/tourism/destinations/Destinations.tsx
@@ -26,7 +26,7 @@ export const Map = () => {
         Tanauan City Hall Location
       </p>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3869.73505047557!2d121.01873887456442!3d14.092811589308065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd709208a54f87%3A0x7d349a8ce58c7f8!2s4232%20Talisay%20-%20Tanauan%20Rd%2C%20Talisay%2C%20Batangas!5e0!3m2!1sen!2sph!4v1710214495844!5m2!1sen!2sph"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3869.735138695966!2d121.01873887509655!3d14.092806386335601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd709208a54f87%3A0x7d349a8ce58c7f8!2s4232%20Talisay%20-%20Tanauan%20Rd%2C%20Talisay%2C%20Batangas!5e0!3m2!1sen!2sph!4v1710819454560!5m2!1sen!2sph"
         allowFullScreen={true}
         loading="lazy"
         className="h-[50vh] w-full md:h-[70vh]"
