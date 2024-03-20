d717432f723f3fcf6d54052a1ab674aa83dbcbc7
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-03-20 14:09:44 +0800

    nn

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./routes/home/Home";
 import {
